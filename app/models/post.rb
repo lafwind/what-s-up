861ab1a5413272e97fe92f6421870db5279b1823
Lafwind <lafwind@gmail.com>
2015-12-16 03:48:52 +0800

    完成 posts#index 页面设计

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,7 +17,7 @@ class Post < ActiveRecord::Base
   URL_REGEX = /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?\z/
 
   validates :title, presence: true, length: {
-              minimum: 6, maximum: 30,
+              minimum: 5, maximum: 30,
               too_long: "%{count} characters is the maximum allowed",
               too_short: "%{count} characters is the minimum allowed"
             }
@@ -28,7 +28,7 @@ class Post < ActiveRecord::Base
             }
 
   validates :description, length: {
-              maximum: 200,
+              maximum: 50,
               too_long: "%{count} characters is the maximum allowed"
             }
 
