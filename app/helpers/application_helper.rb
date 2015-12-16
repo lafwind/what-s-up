456a2b7673d61d598433d9d0da3468d92868612c
Lafwind <lafwind@gmail.com>
2015-12-17 03:02:30 +0800

    添加 footer 设计, 完成 posts#show 页面设计

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def format_url(url)
     url.start_with?('http://', 'https://') ? url : 'http://' + url
   end
+
+  def need_header_or_footer?
+    !(current_page?(new_user_session_path) || current_page?(new_user_registration_path) || current_page?(new_user_password_path) ||
+      current_page?(new_user_confirmation_path) || (params[:controller] == 'static_pages' && params[:action] == 'home'))
+  end
 end
