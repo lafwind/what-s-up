c385098a713dc61fdcb46773047edb4099c0168b
Lafwind <lafwind@gmail.com>
2015-12-18 01:29:05 +0800

    修复一些小 bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     url.start_with?('http://', 'https://') ? url : 'http://' + url
   end
 
-  def need_header_or_footer?
+  def is_need_header_or_footer?
     !(current_page?(new_user_session_path) || current_page?(new_user_registration_path) || current_page?(new_user_password_path) ||
       current_page?(new_user_confirmation_path) || (params[:controller] == 'static_pages' && params[:action] == 'home'))
   end
