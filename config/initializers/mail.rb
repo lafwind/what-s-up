089dd7817f5ed5c7191c20c7f35cbda6c8dcc776
Lafwind <lafwind@gmail.com>
2016-06-02 01:39:24 +0800

    使用 Figaro 管理环境变量

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.gmail.com',
-  port: 587,
-  domain: 'gmail.com',
-  user_name: 'ENV[user_name]',
-  password: 'ENV[password]',
+  address: ENV['SMTP_ADDRESS'],
+  port: ENV['SMTP_PORT'],
+  domain: ENV['SMTP_DOMAIN'],
+  user_name: ENV['SMTP_USER_NAME'],
+  password: ENV['SMTP_PASSWORD'],
   authentication: 'plain',
   enable_starttls_auto: true
 }
