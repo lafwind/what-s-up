fded67d56e1410de6a772a67948dccd1ff680a7f
Lafwind <lafwind@gmail.com>
2016-06-18 02:20:47 +0800

    config devise.rb for sending mails

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,9 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'Lafwind <lafwind@163.com>'
+
+  # config.mailer_sender = 'Lafwind <lafwind@163.com>'
+  config.mailer_sender = 'Lafwind <lafwind@gmail.com>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
