b5ea465d2c60fa2d8765f2d97cf9ab3c1049da58
Lafwind <lafwind@gmail.com>
2016-06-17 17:26:52 +0800

    config environment/production.rb for sending mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,34 +58,34 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "what_s_up_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
-config.active_support.deprecation = :notify
+  config.active_support.deprecation = :notify
 
-# Use default logging formatter so that PID and timestamp are not suppressed.
-config.log_formatter = ::Logger::Formatter.new
+  # Use default logging formatter so that PID and timestamp are not suppressed.
+  config.log_formatter = ::Logger::Formatter.new
 
-# Use a different logger for distributed setups.
-# require 'syslog/logger'
-# config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  # Use a different logger for distributed setups.
+  # require 'syslog/logger'
+  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-if ENV["RAILS_LOG_TO_STDOUT"].present?
-  logger           = ActiveSupport::Logger.new(STDOUT)
-  logger.formatter = config.log_formatter
-  config.logger = ActiveSupport::TaggedLogging.new(logger)
-end
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 
-# Do not dump schema after migrations.
-config.active_record.dump_schema_after_migration = false
+  # Do not dump schema after migrations.
+  config.active_record.dump_schema_after_migration = false
 
-config.action_mailer.default_url_options = { host: 'https://what-s-up.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://what-s-up.herokuapp.com' }
 end
